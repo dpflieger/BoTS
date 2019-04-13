1efd9a40283de2932ed630c91905c0c99f85bec8
David Pflieger <pflieger.dav@gmail.com>
2019-04-13 20:05:24 +0200

    Improved version

--- Discord_bot/bot_mars.py
+++ Discord_bot/bot_mars.py
@@ -1,28 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 import random
 import asyncio
 import aiohttp
 import json
 import discord
 from string import Template
+from discord.ext.commands import Bot
 from discord.ext import commands
 
-
 BOT_PREFIX = ("?", "!")
-TOKEN = open("token.txt", "r").read()
 
-def get_jokes():
-    pass
+with open("token.txt", "r") as f:
+    TOKEN = f.read()
 
-def get_dota_joke():
-    with open("mars.txt", "r") as f:
+bot = Bot(command_prefix=BOT_PREFIX, description="A 'funny' BOT")
+
+def get_joke():
+    """
+    Returns a random joke from the jokes.txt file
+    """
+    with open("jokes.txt", encoding = "UTF-8") as f:
         joke = random.choice(f.readlines())
         return(joke)
 
-bot = commands.Bot(command_prefix=BOT_PREFIX, description='A bot that greets the user back.')
+def get_dota_joke():
+    """
+    Returns a random joke from the mars.txt file
+    The mars.txt is an old heritage of the battle.net bots
+    """
+    with open("mars.txt", encoding = "UTF-8") as f:
+        joke = random.choice(f.readlines())
+        return(joke)
+
+# #https://stackoverflow.com/questions/43963366/discord-py-module-how-do-i-remove-the-command-text-afterwards
+# @bot.event
+# async def on_message(message):
+#     """
+#     Removes command message
+#     """
+#     if message.content.startswith(BOT_PREFIX):
+#         await message.delete()
+#     await bot.process_commands(message)
 
 @bot.event
 async def on_ready():
-    print('Logged in as', bot.user.name, bot.user.id)
+    print("------------------------------------")
+    print("Bot Name: ", bot.user.name)
+    print("Bot ID: ", bot.user.id)
+    print("Discord version: ", discord.__version__)
+    print("------------------------------------")
+
+# see https://stackoverflow.com/questions/52348148/delete-messages-with-python-discord-bot
+@bot.command(pass_context=True)
+async def purge(ctx, limit: int = 20, member: discord.Member = None, *, matches: str = None):
+    """
+    Purge all messages, optionally from `member` or contains `matches`.
+    """
+    def check_msg(msg):
+        if msg.id == ctx.message.id:
+            return True
+        if member is not None:
+            if msg.author.id != member.id:
+                return False
+        if matches is not None:
+            if matches not in msg.content:
+                return False
+        return True
+    deleted = await ctx.channel.purge(limit=limit, check=check_msg)
+    msg = await ctx.send(ctx, 'purge', len(deleted))
+    await asyncio.sleep(2)
+    await msg.delete()
+
 
 @bot.command(pass_context = True)
 async def hello(ctx):
@@ -33,7 +83,6 @@ async def bitcoin(ctx):
     """
     Get the bitcoin price in Euro
     """
-
     url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
     async with aiohttp.ClientSession() as session:  # Async HTTP request
         raw_response = await session.get(url)
@@ -51,30 +100,39 @@ async def ping(ctx):
     await ctx.send(round(latency, 3))
 
 @bot.command()
-async def echo(ctx, *, content:str):
+async def echo(ctx, *, content : str):
     await ctx.send(content)
 
 @bot.command()
-async def cat(ctx):
-    await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
+async def mras(ctx):
+    await ctx.send("https://giphy.com/gifs/mars-dave-chapelle-chapell-show-h3KQUcJ6T3os0")
 
 @bot.command()
+async def masr(ctx):
+    await ctx.send("https://giphy.com/gifs/mars-dave-chapelle-chapell-show-h3KQUcJ6T3os0")
+
+@bot.command()
+async def amrs(ctx):
+    await ctx.send("https://giphy.com/gifs/mars-dave-chapelle-chapell-show-h3KQUcJ6T3os0")
+
+@bot.command()
+async def amsr(ctx):
+    await ctx.send("https://giphy.com/gifs/mars-dave-chapelle-chapell-show-h3KQUcJ6T3os0")
+
+@bot.command(pass_context = True)
 async def mars(ctx):
+    """
+    Return a DotA joke.
+    Similar to the good old !mars command of the battle.net bots :)
+    """
+    random_user = random.choice([user for user in bot.users if user is not ctx.message.author])
+    print(random_user.display_name)
     dota_joke = Template(get_dota_joke())
-    print(dota_joke)
-    dota_joke = dota_joke.substitute(victim = ctx.message.author.display_name, random = ctx.message.author.display_name)
+    dota_joke = dota_joke.substitute(victim = ctx.message.author.display_name, random = random_user)
     await ctx.send(dota_joke)
 
-
-""" @client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
- """
+@bot.command()
+async def joke(ctx):
+    await ctx.send(get_joke())
 
 bot.run(TOKEN)
